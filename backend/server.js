1235db9ddd749c1bb9e4ae75a876c52ccd865310
TeggeAtze <niclaasmoldenhauer@googlemail.com>
2024-07-09 11:12:26 +0200

    added: server setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connect from './src/db/connect.js';
+import cookieParser from 'cookie-parser';
+import fs from 'node:fs';
 
 dotenv.config ();
 
@@ -9,18 +12,35 @@ const port = process.env.PORT || 8000;
 const app = express ();
 
 // middleware
-app.use(
-    cors({
-      origin: process.env.CLIENT_URL,
-      credentials: true,
-    })
-  );
+app.use (
+  cors ({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use (express.json ());
 app.use (express.urlencoded ({extended: true}));
+app.use (cookieParser ());
 
-const server = () => {
+// routes
+const routeFiles = fs.readdirSync ('./src/routes');
+
+routeFiles.forEach (file => {
+  // use dynamic import
+  import (`./src/routes/${file}`)
+    .then (route => {
+      app.use ('/api/v1', route.default);
+    })
+    .catch ((err) => {
+      console.log ('Failed to load route file', err);
+    });
+});
+
+const server = async () => {
   try {
+    await connect ();
+
     app.listen (port, () => {
       console.log (`Server started on port ${port}`);
     });
